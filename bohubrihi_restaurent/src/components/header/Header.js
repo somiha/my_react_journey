03ceb2fe027bdc0ca3a215a0f29219968ec6a140
Somiha <stasnimjes@gmail.com>
2021-05-23 14:41:19 +0600

    Navigation bar using Reactstrap

--- bohubrihi_restaurent/src/components/header/Header.js
+++ bohubrihi_restaurent/src/components/header/Header.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Navigation from './Navigation.js';
 
 
 const Header = () => {
     return (
         <div>
-            
+            <Navigation />
         </div>
     )
 }
